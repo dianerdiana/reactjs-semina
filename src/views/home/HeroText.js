ed9ef64313b671c48418a0fb4a5612ffb3234e68
dianerdiana <dianerdiana.de@gmail.com>
2023-05-14 18:46:21 +0700

    Breakdown to small components Home

---
+++ src/views/home/HeroText.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+// ** RB
+import { Row, Col } from 'react-bootstrap'
+
+// ** Text
+import knowledge from '../../assets/images/text/knowledge.png'
+import joining from '../../assets/images/text/joining.png'
+
+const HeroText = () => {
+  return (
+    <Row className="justify-content-center mb-8">
+      <Col xs="12" className="py-md-3 py-0"></Col>
+      <Col sm="8" className="text-center py-6">
+        <span className="text-white hero-title">
+          Expand Your <img src={knowledge} alt="knowledge" className="text-knowledge" />
+          <br />
+          by <img src={joining} alt="joining" className="text-joining" /> Our Greatest
+          Events
+        </span>
+      </Col>
+      <Col sm="8" className="text-center">
+        <span className="text-gray">
+          Kami menyediakan berbagai acara terbaik untuk membantu{' '}
+          <br className="d-none d-md-inline" />
+          anda dalam meningkatkan skills di bidang teknologi
+        </span>
+      </Col>
+    </Row>
+  )
+}
+
+export default HeroText
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,32 +1,17 @@
 import React from 'react'
-import { Row, Col, Container } from 'react-bootstrap'
-import Carousel from './Carousel'
-import knowledge from '../../assets/images/text/knowledge.png'
-import joining from '../../assets/images/text/joining.png'
 
-const Landing = () => {
+// ** RB
+import { Container } from 'react-bootstrap'
+
+// ** Components
+import Carousel from './Carousel'
+import HeroText from './HeroText'
+
+const Home = () => {
   return (
     <>
       <Container>
-        <Row className="justify-content-center mb-8">
-          <Col xs="12" className="py-md-3 py-0"></Col>
-          <Col sm="8" className="text-center py-6">
-            <span className="text-white hero-title">
-              Expand Your{' '}
-              <img src={knowledge} alt="knowledge" className="text-knowledge" />
-              <br />
-              by <img src={joining} alt="joining" className="text-joining" /> Our Greatest
-              Events
-            </span>
-          </Col>
-          <Col sm="8" className="text-center">
-            <span className="text-gray">
-              Kami menyediakan berbagai acara terbaik untuk membantu{' '}
-              <br className="d-none d-md-inline" />
-              anda dalam meningkatkan skills di bidang teknologi
-            </span>
-          </Col>
-        </Row>
+        <HeroText />
         <Carousel />
       </Container>
       <div
@@ -41,4 +26,4 @@ const Landing = () => {
   )
 }
 
-export default Landing
+export default Home
