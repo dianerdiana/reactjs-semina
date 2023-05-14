553aeb6866364610d9c66882e0d4b0301389ebea
dianerdiana <dianerdiana.de@gmail.com>
2023-05-14 16:56:36 +0700

    Create carrousel comp

--- src/views/Landing/Carousel.js
+++ src/views/Landing/Carousel.js
@@ -1,43 +1,42 @@
 // ** Third Party Components
-import Slider from 'react-slick'
-import { Button, Card } from 'react-bootstrap'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import { Carousel } from 'react-bootstrap'
 
-const Carousel = () => {
+// ** Images
+import image_1 from '../../assets/images/carousel/image-3.jpg'
+import image_2 from '../../assets/images/carousel/image-2.jpg'
+import image_3 from '../../assets/images/carousel/image-2.jpg'
+
+const CarouselComponent = () => {
+  const data = [
+    {
+      id: 1,
+      img: image_1
+    },
+    {
+      id: 2,
+      img: image_2
+    },
+    {
+      id: 3,
+      img: image_3
+    }
+  ]
+
   return (
-    <>
-      <div className="slideshow-container">
-        <div className="mySlides fade">
-          <div className="numbertext">1 / 3</div>
-          <img src="img1.jpg" />
-          <div className="text">Caption Text</div>
-        </div>
-
-        <div className="mySlides fade">
-          <div className="numbertext">2 / 3</div>
-          <img src="img2.jpg" />
-          <div className="text">Caption Two</div>
-        </div>
-
-        <div className="mySlides fade">
-          <div className="numbertext">3 / 3</div>
-          <img src="img3.jpg" />
-          <div className="text">Caption Three</div>
-        </div>
-
-        <a className="prev">&#10094;</a>
-        <a className="next">&#10095;</a>
-      </div>
-      <br />
-
-      <div className="text-center">
-        <span className="dot" onclick="currentSlide(1)"></span>
-        <span className="dot" onclick="currentSlide(2)"></span>
-        <span className="dot" onclick="currentSlide(3)"></span>
-      </div>
-    </>
+    <Carousel interval={5000} variant="light" className="text-center mb-8">
+      {data.map((crs) => {
+        return (
+          <Carousel.Item key={crs.id}>
+            <img
+              src={crs.img}
+              alt="Carousel"
+              className="rounded rounded-4 img-carousel"
+            />
+          </Carousel.Item>
+        )
+      })}
+    </Carousel>
   )
 }
 
-export default Carousel
+export default CarouselComponent
